067145d143d7da0660d3c44951f716393c81c0bb
Hazique Quazi <hazique.q@gmail.com>
2020-03-07 13:34:25 +0530

    Add files via upload

---
+++ StepsToReduceANoToZero.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+
+
+var numberOfSteps  = function(num) {
+    
+    var stepCount = 0;
+    
+    if ( num >= 0 && num <= 1000000 ){
+        
+        while ( num !== 0 ){
+            
+            stepCount = stepCount + 1;
+            num = countSteps(num);
+        }
+    }      
+    return stepCount;
+};
+
+var countSteps = function(num) {
+    if ( num%2 == 0 )
+        return num/2;
+    
+    else 
+        return num - 1;
+    
+}
